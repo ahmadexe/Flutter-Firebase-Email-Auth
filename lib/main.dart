892d6fba9b3f8a6f7a19eef05828b7ded6e1d096
Ahmad <muahmad710@gmail.com>
2022-04-19 12:44:30 +0500

    Auth setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_email_auth/screens/home_screen.dart';
+import 'package:flutter_email_auth/screens/login_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
