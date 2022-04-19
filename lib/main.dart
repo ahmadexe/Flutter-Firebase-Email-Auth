1c446fd8cd2d545f9bc68c75919f861b64fc3380
Ahmad <muahmad710@gmail.com>
2022-04-19 12:43:37 +0500

    Auth setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_email_auth/screens/home_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("JEJE"),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges() ,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+          else {
+            return LoginPage();
+          }
+        },
+      )
     );
   }
 }
