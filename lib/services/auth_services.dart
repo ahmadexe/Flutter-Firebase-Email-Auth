b5f8a5bd9b6f00e2f8b8b8e6643b7541420b4c58
Ahmad <muahmad710@gmail.com>
2022-04-19 13:56:26 +0500

    Just cleaning stuff

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,16 +1,17 @@
-
 import 'dart:math';
 
 import 'package:firebase_auth/firebase_auth.dart';
-class AuthServices{
+
+class AuthServices {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-    Future SignIn({required email, required password}) async {
-    try{
+  Future SignIn({required email, required password}) async {
+    try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
     } catch (e) {
       print(e.toString());
     }
   }
+
   SignOut() {
     _auth.signOut();
   }
