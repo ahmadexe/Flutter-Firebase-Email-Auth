5b27b96f5f54ade44d0ebf12a8912c66227f997a
Ahmad <muahmad710@gmail.com>
2022-04-19 14:52:46 +0500

    Adding sign up

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../services/auth_services.dart';
+
+class SignUp extends StatelessWidget {
+  SignUp({ Key? key }) : super(key: key);
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  AuthServices _auth = AuthServices();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
