8268c1904ca009c9ba6b81d526a9d5d28eba3954
Ahmad <muahmad710@gmail.com>
2022-04-19 12:47:09 +0500

    Auth setup

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,6 +1,15 @@
 
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 class AuthServices{
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  
+  Future SignIn({required email, required password}) async {
+    try{
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      print(e.toString());
+    }
+
+  }
 }
