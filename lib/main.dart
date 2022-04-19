4ac9fd333601c8b0b815662586e926c338eff8ce
Ahmad <muahmad710@gmail.com>
2022-04-19 13:58:22 +0500

    Just cleaning stuff

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,23 @@ Future main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges() ,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return LoginPage();
-          }
-          else {
-            return HomePage();
-          }
-        },
-      )
-      // home: HomePage(),
-      // home: LoginPage(),
-    );
+        home: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return LoginPage();
+        } else {
+          return HomePage();
+        }
+      },
+    )
+        // home: HomePage(),
+        // home: LoginPage(),
+        );
   }
 }
