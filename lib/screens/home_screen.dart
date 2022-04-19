e04e26e1ba92a60e3cd27634be637efaf8a29663
Ahmad <muahmad710@gmail.com>
2022-04-19 13:09:03 +0500

    Setting up screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,36 +9,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 100,
-          ),
-          TextField(
-            controller: emailController,
-            textInputAction: TextInputAction.next,
-            decoration: const InputDecoration(labelText: "Email", hintText: "Enter Your email"),
-          ),
-          TextField(
-            controller: passwordController,
-            obscureText: true,
-            textInputAction: TextInputAction.done,
-            decoration: const InputDecoration(labelText: "Password", hintText: "Enter Your Password"),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-
-          Align(alignment: Alignment.center, child: Container(
-            width: 70,
-            height: 60,
-            color: Colors.blue,
-            child: Align(alignment: Alignment.center, child: Text("LOGIN"),),
-          ),)
-          
-        ],
+      child: Padding(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            TextField(
+              controller: emailController,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: "Email", hintText: "Enter Your email"),
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(labelText: "Password", hintText: "Enter Your Password"),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+      
+            Align(alignment: Alignment.center, child: InkWell(
+              onTap: () {
+                
+              },
+              child: Container(
+                width: 70,
+                height: 60,
+                color: Colors.blue,
+                child: Align(alignment: Alignment.center, child: Text("LOGIN"),),
+              ),
+            ),)
+            
+          ],
+        ),
       ),
     );
   }
--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -4,7 +4,7 @@ import 'dart:math';
 import 'package:firebase_auth/firebase_auth.dart';
 class AuthServices{
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  Future SignIn({required email, required password}) async {
+    Future SignIn({required email, required password}) async {
     try{
       await _auth.signInWithEmailAndPassword(email: email, password: password);
     } catch (e) {
