a670d8661d8026c3b558ba823bfc228f13307b7b
Ahmad <muahmad710@gmail.com>
2022-04-19 12:45:37 +0500

    Auth setup

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,5 +1,6 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 class AuthServices{
-  static final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  
 }
